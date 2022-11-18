3762d72cb0540b8fbac13cf8fca0080555f9f283
Aziz <kayumovabduaziz@gmail.com>
2022-11-18 13:20:02 +0900

    choose split axis &  index

--- src/algorithm/split.rs
+++ src/algorithm/split.rs
@@ -1,57 +1,63 @@
 use crate::node::Node;
-use ordered_float::Float;
-use std::ops::{AddAssign, DivAssign, MulAssign, SubAssign};
+use ordered_float::{Float, OrderedFloat};
+use std::{
+    fmt::Debug,
+    ops::{AddAssign, DivAssign, MulAssign, SubAssign},
+};
 
-fn calculate_mean<T>(points: &Vec<Vec<T>>) -> Vec<T>
-where T: Float + AddAssign + SubAssign + MulAssign + DivAssign,
+fn calculate_mean<T>(points: &Vec<Vec<T>>, from: usize, end: usize) -> Vec<T>
+where
+    T: Debug + Float + AddAssign + SubAssign + MulAssign + DivAssign,
 {
     if points.is_empty() {
         return Vec::new();
     }
     let mut number_of_points = T::zero();
     let mut mean: Vec<T> = vec![T::zero(); points[0].len()];
-    points.iter().for_each(|point| {
-        for i in 0..point.len() {
-            mean[i] += point[i];
+    for i in from..end {
+        for j in 0..points[i].len() {
+            mean[j] += points[i][j];
         }
         number_of_points += T::one();
-    });
+    }
     for i in 0..mean.len() {
         mean[i] /= number_of_points;
     }
     mean
 }
 
-fn calculate_variance<T>(points: &Vec<Vec<T>>, mean: &Vec<T>) -> Vec<T>
-where T: Float + AddAssign + SubAssign + MulAssign + DivAssign,
+fn calculate_variance<T>(points: &Vec<Vec<T>>, from: usize, end: usize) -> Vec<T>
+where
+    T: Debug + Float + AddAssign + SubAssign + MulAssign + DivAssign,
 {
     if points.is_empty() {
         return Vec::new();
     }
+
+    let mean = calculate_mean(points, from, end);
     let mut number_of_points = T::zero();
-    let mut variance = vec![T::zero(); points[0].len()];
-    points.iter().for_each(|point| {
-        for i in 0..point.len() {
-            variance[i] += (point[i] - mean[i]).powi(2);
+    let mut variance = vec![T::zero(); mean.len()];
+
+    for i in from..end {
+        for j in 0..points[i].len() {
+            variance[j] += (points[i][j] - mean[j]).powi(2);
         }
         number_of_points += T::one();
-    });
+    }
     for i in 0..mean.len() {
         variance[i] /= number_of_points;
     }
     variance
 }
 
-fn choose_split_dimension<T>(node: &Node<T>) -> usize
-where T: Float + AddAssign + SubAssign + MulAssign + DivAssign,
+fn choose_split_axis<T>(node: &Node<T>) -> usize
+where
+    T: Debug + Float + AddAssign + SubAssign + MulAssign + DivAssign,
 {
-    // 1. Calculate mean for each dimension:
-    let mean = calculate_mean(node.points());
+    // 1. Calculate variance for each axis:
+    let variance = calculate_variance(node.points(), 0, node.points().len());
 
-    // 2. Find the sum of squared difference for each dimension and divide by number_of_points:
-    let variance = calculate_variance(node.points(), &mean);
-
-    // 3. Choose the dimension with the highest variance
+    // 2. Choose the axis with the highest variance
     let mut selected_index = 0;
     for i in 0..variance.len() {
         if variance[i] > variance[selected_index] {
@@ -61,11 +67,52 @@ where T: Float + AddAssign + SubAssign + MulAssign + DivAssign,
     selected_index
 }
 
-fn choose_split_index<T>(node: &Node<T>, dimension: usize) -> usize {
-    0
+fn choose_split_index<T>(
+    node: &mut Node<T>,
+    min_number_of_elements: usize,
+    max_number_of_elements: usize,
+) -> usize
+where
+    T: Debug + Float + AddAssign + SubAssign + MulAssign + DivAssign,
+{
+    if node.points().len() < 2 * min_number_of_elements {
+        panic!("Cannot split as there're less elements than 2 * min_number_of_elements");
+    }
+
+    // 1. Choose the split axis
+    let axis = choose_split_axis(node);
+
+    // 2. Sort node points along that axis
+    node.points_mut().sort_by_key(|p| OrderedFloat(p[axis]));
+
+    // 3. Minimize the sum of variances for two groups of node.points
+    let mut selected_index = min_number_of_elements;
+    let mut min_variance = T::infinity();
+
+    let number_of_elements = node.points().len();
+    let start = min_number_of_elements;
+    let end = max_number_of_elements.min(number_of_elements - min_number_of_elements) + 1;
+    for i in start..end {
+        let mut current_variance = T::zero();
+        calculate_variance(node.points(), 0, i)
+            .iter()
+            .for_each(|v| {
+                current_variance += v.clone();
+            });
+        calculate_variance(node.points(), i, node.points().len())
+            .iter()
+            .for_each(|v| {
+                current_variance += v.clone();
+            });
+        if current_variance < min_variance {
+            min_variance = current_variance;
+            selected_index = i;
+        }
+    }
+    selected_index
 }
 
-pub fn split<'a, T>(node: &'a mut Node<T>) -> Option<Node<T>> {
+pub fn split<T>(node: &mut Node<T>) -> Option<Node<T>> {
     None
 }
 
@@ -84,33 +131,40 @@ mod tests {
     }
 
     #[test]
-    pub fn test_mean_calculation(){
+    pub fn test_mean_calculation() {
         let points = get_test_points();
-        let mean = calculate_mean(&points);
+        let mean = calculate_mean(&points, 0, points.len());
         assert_eq!(mean[0], 0.);
         assert_eq!(mean[1], 4.);
     }
 
     #[test]
-    pub fn test_mean_variance(){
+    pub fn test_variance_calculation() {
         let points = get_test_points();
-        let mean = calculate_mean(&points);
-        let variance = calculate_variance(&points, &mean);
+        let variance = calculate_variance(&points, 0, points.len());
         assert_eq!(variance[0], 0.);
         assert_eq!(variance[1], 14.);
     }
 
     #[test]
-    pub fn test_choose_split_dimension() {
+    pub fn test_range_variance_calculation() {
+        let points = get_test_points();
+        let variance = calculate_variance(&points, 0, 2);
+        assert_eq!(variance[0], 0.);
+        assert_eq!(variance[1], 0.25);
+    }
+
+    #[test]
+    pub fn test_choose_split_axis() {
         let origin = vec![0., 0.];
         let mut node = Node::new_leaf(&origin, 10);
         get_test_points().iter().for_each(|point| {
             node.points_mut().push(point.to_owned());
         });
 
-        let expected_dimension = 1;
-        let selected_dimension = choose_split_dimension(&node);
-        assert_eq!(expected_dimension, selected_dimension);
+        let expected_axis = 1;
+        let selected_axis = choose_split_axis(&node);
+        assert_eq!(expected_axis, selected_axis);
     }
 
     #[test]
@@ -122,7 +176,7 @@ mod tests {
         });
 
         let expected_index = 3;
-        let selected_index = choose_split_index(&node, 1);
+        let selected_index = choose_split_index(&mut node, 2, 3);
         assert_eq!(expected_index, selected_index);
     }
 }
