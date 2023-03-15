bf113fdba3be7a04d4df86ce440a6e72fd46ce90
Aziz Kayumov <kayumovabduaziz@gmail.com>
2023-03-15 19:10:01 +0900

    fix exhaustive search to use quickselect

--- bench/benches/clustered/mod.rs
+++ bench/benches/clustered/mod.rs
@@ -3,3 +3,7 @@ pub use world_cities::{
     bulkload::world_cities_benchmark as world_cities_bulkload_benchmark,
     sequential::world_cities_benchmark as world_cities_sequential_benchmark,
 };
+mod dns_traffic;
+pub use dns_traffic::{
+    sequential::dns_traffic_benchmark as dns_traffic_sequential_benchmark,
+};
