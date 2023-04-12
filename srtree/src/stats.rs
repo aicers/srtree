0c1f75d8825db492b0941b5f3fbcad0bd6a8fb06
azizkayumov <kayumovabduaziz@gmail.com>
2023-04-12 16:35:17 +0900

    regroup partitions into groups to fix #23

--- srtree/src/stats.rs
+++ srtree/src/stats.rs
@@ -6,7 +6,6 @@ use num_traits::Float;
 
 use crate::node::Node;
 
-const STATS_ENABLED: bool = true;
 static mut num_visited_nodes: usize = 0;
 static mut num_visited_leaves: usize = 0;
 static mut num_compared_nodes: usize = 0;
@@ -64,7 +63,34 @@ where T: Float + AddAssign + SubAssign + MulAssign + DivAssign + Debug + Copy
             println!("Visited nodes:   {}", num_visited_nodes);
             println!("Compared nodes:  {}", num_compared_nodes);
             println!("Total leaves:    {}", root.leaf_count());
-            println!("Total nodes:     {}", root.node_count());
+            println!("Total nodes:     {} (including leaf nodes)", root.node_count());
+            println!("Tree height:     {}", root.get_height());
         }
+    }
+}
+
+const STATS_ENABLED: bool = true;
+
+#[cfg(test)]
+mod tests {
+    use crate::{SRTree, Params};
+    use rand::{rngs::StdRng, Rng, SeedableRng};
+
+    #[test]
+    pub fn test_bulk_load() {
+        let mut rng = StdRng::from_seed(*b"PiH6Xi3GBBXhTK6UsXJYngHaF3fx4aYS");
+        const D: usize = 8; // dimension
+        const N: usize = 10000; // number of points
+        let mut pts = Vec::new();
+        for _ in 0..N {
+            let mut point = [0.; D];
+            for item in point.iter_mut().take(D) {
+                *item = rng.gen::<f64>();
+            }
+            pts.push(point);
+        }
+        let pts: Vec<Vec<f64>> = pts.iter().map(|p| p.to_vec()).collect();
+        let tree = SRTree::bulk_load(&pts, Params::default_params());
+        tree.query(&pts[0], 15);
     }
 }
