3afa2db516acbd268fcf319e0a90963fa88a7cfa
azizkayumov <kayumovabduaziz@gmail.com>
2023-05-22 17:03:51 +0900

    check distances not indices

--- srtree/tests/test_query.rs
+++ srtree/tests/test_query.rs
@@ -39,19 +39,20 @@ fn test_with_random_points() {
     let mut points = pts.clone();
     for p in pts.iter() {
         // Bulk-loaded SRTree nearest neighbors
-        let result_bulk = bulk_tree.query(p, k);
-        assert_eq!(result_bulk.0.len(), k);
+        let (bulk_indices, bulk_distances) = bulk_tree.query(p, k);
+        assert_eq!(bulk_indices.len(), k);
 
         // Sequential SRTree nearest neighbors
-        let result_sequential = sequential_tree.query(p, k);
-        assert_eq!(result_sequential.0.len(), k);
+        let (seq_indices, seq_distances) = sequential_tree.query(p, k);
+        assert_eq!(seq_indices.len(), k);
 
         // Brute-force
         points.sort_by_key(|a| OrderedFloat(euclidean_squared(a, p)));
 
         for i in 0..k {
-            assert_eq!(pts[result_bulk.0[i]], points[i]);
-            assert_eq!(pts[result_sequential.0[i]], points[i]);
+            let distance_brute_force = euclidean_squared(&points[i], p).sqrt();
+            assert_eq!(bulk_distances[i], distance_brute_force);
+            assert_eq!(seq_distances[i], distance_brute_force);
         }
     }
 }
